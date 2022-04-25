8e14253288ce0e57228b396545edcca90c7ede57
VinzHostingg <103474345+VinzHostingg@users.noreply.github.com>
2022-04-25 14:31:27 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -14,12 +14,12 @@ echo
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo $i" |"$cy"     INDONESIA DARK TERMUX ASSOCIATE"$i"         |"
 echo $i" |"$me"─────────────────────────────────────────────"$i"|"
-echo $i" |"$pu" AUTHOR :"$ku" RISKI DARMAWAN  [FR13ND8]"$i"          |"
-echo $i" |"$pu" FACEBOOK :"$ku" Riski Darmawan"$i"                   |"
-echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"  |"
+echo $i" |"$pu" AUTHOR :"$ku" VINZ STORE  [FR13ND8]"$i"          |"
+echo $i" |"$pu" FACEBOOK :"$ku" kepo kamu"$i"                   |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.V1NZ] [MR.S∆MAx] [MR.B]"$i"  |"
 echo $i" |"$cy"            ~ [cyber] [%MR.S%] [AldykaPrygh]"$i" |"
-echo $i" |"$pu" Contack WhatsApp :"$ku" 085835787069"$i"             |"
-echo $i" |"$pu" Contack Gmail :"$ku" dermawan0858@gmail.com"$i"      |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 085891371231"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" pratamaputrakevin42@gmail.com"$i"      |"
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
 echo $i"────────────────────────────────    ─────"
@@ -255,8 +255,8 @@ echo $cy"Terima Kasih Sudah Pakai Tool Saya"
 sleep 2
 echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
 sleep 2
-echo $ku"WhatsApp :"$i" 085835787069"
-echo $ku"Facebook :"$i" Riski Darmawan"
+echo $ku"WhatsApp :"$i" 085891371231"
+echo $ku"Facebook :"$i" kepo yaa"
 sleep 2
 echo $pur">> Thanks Yang Sudah Support Saya <<"
 exit
